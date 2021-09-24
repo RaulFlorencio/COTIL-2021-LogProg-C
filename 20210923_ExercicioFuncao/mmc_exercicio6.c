86993a8bf9e50496afaa8978a0cf27d3cb851909
RaulFlorencio <raulflorencio866@gmail.com>
2021-09-23 22:58:55 -0300

    exercicio5funcao

---
+++ 20210923_ExercicioFuncao/mmc_exercicio6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int resto_divisao(int n1, int n2){
+    int resto = 0;
+    if(n1>n2)
+        resto = n1%n2;
+    else
+        resto = n2%n1;
+    return resto;
+}
+
+int main()
+{
+    int num1, num2, resto;
+    printf("Digite os números :");
+    scanf("%d,%d", &num1, &num2);
+    resto = resto_divisao(num1, num2);
+    while (resto != 0){
+        num1 = num2;
+        num2 = resto;
+        resto = resto_divisao(num1, num2);
+    }
+    printf("O divisor da divisão exata é %d", num2);
+    printf("\nO resto %d", resto);
+    return 0;
+}
